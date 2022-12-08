0b57699368c2d71d16e7b42ecf1b8f42433b1fc2
Nawaz <nzan576@gmail.com>
2022-12-08 21:26:08 +0530

    UpdateNotes Design implemented

--- app/src/main/java/com/coetusstudio/notes/SplashActivity.java
+++ app/src/main/java/com/coetusstudio/notes/SplashActivity.java
@@ -15,6 +15,8 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        getSupportActionBar().hide();
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
---
+++ app/src/main/java/com/coetusstudio/notes/UpdateNotes.java
@@ -0,0 +1,13 @@
+package com.coetusstudio.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_notes);
+    }
+}
